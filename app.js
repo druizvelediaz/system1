7f031adbea7bbc93cbe20d69b4823eb97263a194
druizvelediaz <druizvelediaz2023@francisparker.org>
2019-09-13 21:47:50 -0700

    Started work on loading in objects

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ function loadDoc() {
         if (this.readyState == 4 && this.status == 200) {
             xhttp = this.responseText;
             console.log(xhttp);
+            doStuff(xhttp);
         }
     };
     xhttp.open("get","https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/ironMan.txt",true);
     xhttp.send();
+}
 
-
-    // var picNameArr = whatever.split("/");
-    // for(var i = 0; i < picNameArr.length; i++){
-    //     var image = $(`<img src="https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/${picNameArr[i]}">`);
-    //     document.getElementById('demo').append(image);
-    // }
+function doStuff(content){
+    var picNameArr = content.split("/");
+    for(var i = 0; i < picNameArr.length; i++){
+        var url = `<img src="https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/${picNameArr[i]}"></img>`;
+        console.log(picNameArr[i],url);
+        var image = $(url);
+        $("#demo").append(image);
+    }
 }
