64a2cba3a5f67bf0290694442d5224693c117e0c
druizvelediaz <druizvelediaz2023@francisparker.org>
2019-09-13 21:26:23 -0700

    updating txt

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+var fileName;
+
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML =
-            this.responseText;
+            xhttp = this.responseText;
+            console.log(xhttp);
         }
     };
-    xhttp.open("GET", "https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/ironMan.txt", true);
+    xhttp.open("get","https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/ironMan.txt",true);
     xhttp.send();
+
+
+    // var picNameArr = whatever.split("/");
+    // for(var i = 0; i < picNameArr.length; i++){
+    //     var image = $(`<img src="https://raw.githubusercontent.com/druizvelediaz/system1/master/Cards/${picNameArr[i]}">`);
+    //     document.getElementById('demo').append(image);
+    // }
 }
